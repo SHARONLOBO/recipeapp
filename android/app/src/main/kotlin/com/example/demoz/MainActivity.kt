daabb5c2cfb99ae78f46d78ee25b25148a86188a
SHARONLOBO <sharonjudithlobo@gmail.com>
2021-07-28 15:40:21 +0530

    recipes

---
+++ android/app/src/main/kotlin/com/example/demoz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demoz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,62 @@
+import 'package:demoz/models/meal.dart';
+import 'package:flutter/material.dart';
+import 'package:demoz/widgets/meal_item.dart';
+
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const routeName = '/category-meals';
+  final List<Meal> availmeals;
+  CategoryMealsScreen(this.availmeals);
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal>displayMeals;
+  bool _loadedInitData=false;
+  @override
+  void didChangeDependencies() {
+    if(!_loadedInitData){
+      final routeArgs =
+      ModalRoute.of(context).settings.arguments as Map<String, String>;
+      categoryTitle = routeArgs['title'];
+      final categoryId = routeArgs['id'];
+      displayMeals = widget.availmeals.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData=true;// TODO: implement didChangeDependencies
+
+    }
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId){
+    setState(() {
+      displayMeals.removeWhere((meal) => meal.id==mealId);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: displayMeals[index].id,
+            title: displayMeals[index].title,
+            imageUrl: displayMeals[index].imageUrl,
+            duration: displayMeals[index].duration,
+            affordability: displayMeals[index].affordability,
+            complexity: displayMeals[index].complexity,
+            removeItem: _removeMeal,
+          );
+        },
+        itemCount: displayMeals.length,
+      ),
+    );
+  }
+}
